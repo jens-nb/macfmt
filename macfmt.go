83f7fd73fc6cc9a562b5be8b277a6f0c37fc25ca
Jens Neuerburg <jens.neuerburg@hiz-saarland.de>
2022-12-26 18:26:44 +0100

    is now pointless

--- macfmt.go
+++ macfmt.go
@@ -19,10 +19,10 @@ func usage() {
 
 // isAllowedCharacter checks if a rune is one of the allowed characters
 // in a MAC address.
-func isAllowedCharacter(r *rune) bool {
+func isAllowedCharacter(r rune) bool {
 	allowedCharacters := "abcdefABCDEF0123456789:.-"
 
-	if !strings.ContainsRune(allowedCharacters, *r) {
+	if !strings.ContainsRune(allowedCharacters, r) {
 		fmt.Println("Error: MAC address contains illegal character.")
 		return false
 	}
@@ -34,15 +34,15 @@ func isAllowedCharacter(r *rune) bool {
 // Allowed positions of separators: 2,4,6,8,10, also
 // Longest possible MAC has 17 chars: AB:CD:EF:78:90:12
 // shortest possible MAC has 12 chars: ABCDEF012345
-func isValidMAC(userInput *string) bool {
+func isValidMAC(userInput string) bool {
 
-	for _, r := range *userInput {
-		if !isAllowedCharacter(&r) {
+	for _, r := range userInput {
+		if !isAllowedCharacter(r) {
 			return false
 		}
 	}
 
-	if len(*userInput) < 12 || len(*userInput) > 17 {
+	if len(userInput) < 12 || len(userInput) > 17 {
 		fmt.Println("Error: MAC address has illegal length.")
 		return false
 	}
@@ -51,16 +51,16 @@ func isValidMAC(userInput *string) bool {
 
 // sanitizeInputMAC removes all separators from an input MAC address
 // and converts all letters to lowercase.
-func sanitizeInputMAC(userInput *string) string {
+func sanitizeInputMAC(userInput string) string {
 	separators := ":.-"
 
-	if strings.ContainsAny(*userInput, separators) {
+	if strings.ContainsAny(userInput, separators) {
 
 		for _, r := range separators {
-			*userInput = strings.ReplaceAll(*userInput, string(r), "")
+			userInput = strings.ReplaceAll(userInput, string(r), "")
 		}
 	}
-	return strings.ToLower(*userInput)
+	return strings.ToLower(userInput)
 }
 
 // formatMAC takes a sanitized MAC as input and returns (prints) a MAC address in the specified format.
@@ -74,7 +74,7 @@ func formatMAC(sanitizedInput string, format string) {
 		s1 := sanitizedInput[0:4]
 		s2 := sanitizedInput[4:8]
 		s3 := sanitizedInput[8:12]
-		substrings = append(substrings, s1,s2,s3) 
+		substrings = append(substrings, s1, s2, s3)
 
 		fmt.Println(strings.Join(substrings, "."))
 	}
@@ -82,15 +82,15 @@ func formatMAC(sanitizedInput string, format string) {
 
 func main() {
 
-	if len(os.Args) < 3{
+	if len(os.Args) < 3 {
 		usage()
 		os.Exit(1)
 	}
 
 	mac := readInputMAC()
 
-	if isValidMAC(&mac) {
-		sanitizedmac := sanitizeInputMAC(&mac)
+	if isValidMAC(mac) {
+		sanitizedmac := sanitizeInputMAC(mac)
 		formatMAC(sanitizedmac, os.Args[2])
 	}
 }
