66a1f68ba17c48cb4eeb63269e43e680b37c9f3d
Jens Neuerburg <jens.neuerburg@hiz-saarland.de>
2022-12-29 16:42:40 +0100

    refactoring

--- util/util.go
+++ util/util.go
@@ -6,15 +6,15 @@ import (
 
 // Split a string into equal sized chunks specified by size.
 // Cisco format MAC addresses need 3x4 chunks, others 6x2.
-func Chunk(input string, size int) ([]string, error) {
+func Chunk(s string, size int) ([]string, error) {
 
-	if len(input)%size != 0 {
-		return nil, errors.New("Error: input not divisible by chunk size.")
+	if len(s)%size != 0 {
+		return nil, errors.New("Chunk error: s not divisible by chunk size.")
 	}
 
 	var result []string
-	for i := 0; i < len(input); i += size {
-		chunk := input[i : i+size]
+	for i := 0; i < len(s); i += size {
+		chunk := s[i : i+size]
 		result = append(result, chunk)
 
 	}
